8f72ff42f57fa1d015fc0384e3c8dbe76f58fb7a
Phrase <support@phrase.com>
2020-12-28 08:42:49 +0000

    Deploying from  phrase/openapi@6ed00a65

--- test/test_project_update_parameters.py
+++ test/test_project_update_parameters.py
@@ -35,12 +35,29 @@ class TestProjectUpdateParameters(unittest.TestCase):
         # model = phrase_api.models.project_update_parameters.ProjectUpdateParameters()  # noqa: E501
         if include_optional :
             return ProjectUpdateParameters(
+                account_id = 'abcd1234', 
                 name = 'My Android Project', 
                 main_format = 'yml', 
                 shares_translation_memory = True, 
                 project_image = bytes(b'blah'), 
-                remove_project_image = True, 
-                account_id = 'abcd1234'
+                remove_project_image = False, 
+                workflow = 'review', 
+                machine_translation_enabled = True, 
+                enable_branching = True, 
+                protect_master_branch = True, 
+                enable_all_data_type_translation_keys_for_translators = True, 
+                enable_icu_message_format = True, 
+                zero_plural_form_enabled = True, 
+                autotranslate_enabled = True, 
+                autotranslate_check_new_translation_keys = True, 
+                autotranslate_check_new_uploads = True, 
+                autotranslate_check_new_locales = True, 
+                autotranslate_mark_as_unverified = True, 
+                autotranslate_use_machine_translation = True, 
+                autotranslate_use_translation_memory = True, 
+                smart_suggest_enabled = True, 
+                smart_suggest_use_glossary = True, 
+                smart_suggest_use_machine_translation = True
             )
         else :
             return ProjectUpdateParameters(

1b3b33732ec0afb84750e9dfb3c84119a1503b30
Phrase <support@phrase.com>
2023-10-13 07:53:26 +0000

    Deploying from  phrase/openapi@9b60f7c8

--- phrase_api/configuration.py
+++ phrase_api/configuration.py
@@ -370,7 +370,7 @@ class Configuration(object):
                "OS: {env}\n"\
                "Python Version: {pyversion}\n"\
                "Version of the API: 2.0.0\n"\
-               "SDK Package Version: 1.13.0".\
+               "SDK Package Version: 1.14.0".\
                format(env=sys.platform, pyversion=sys.version)
 
     def get_host_settings(self):

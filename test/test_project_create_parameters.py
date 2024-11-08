fd3a920d7789e1890bd9289b65b8b1ba95957818
Phrase <support@phrase.com>
2024-11-08 09:39:34 +0000

    Deploying from  phrase/openapi@b1086653

--- test/test_project_create_parameters.py
+++ test/test_project_create_parameters.py
@@ -41,7 +41,7 @@ class TestProjectCreateParameters(unittest.TestCase):
                 main_format = 'yml', 
                 media = 'Python', 
                 shares_translation_memory = True, 
-                project_image = '[B@639f7054', 
+                project_image = '[B@11f8bff1', 
                 remove_project_image = True, 
                 account_id = 'abcd1234', 
                 point_of_contact = 'abcd1234', 

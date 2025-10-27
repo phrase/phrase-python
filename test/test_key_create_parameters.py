c4937919fce15b993ae4de9d1d3f321dc628ebae
Phrase <support@phrase.com>
2025-10-27 14:14:08 +0000

    Deploying from  phrase/openapi@690a2ce9

--- test/test_key_create_parameters.py
+++ test/test_key_create_parameters.py
@@ -46,7 +46,7 @@ class TestKeyCreateParameters(unittest.TestCase):
                 data_type = 'number', 
                 tags = 'awesome-feature,needs-proofreading', 
                 max_characters_allowed = 140, 
-                screenshot = '[B@566bea82', 
+                screenshot = '[B@5fcf9bff', 
                 remove_screenshot = True, 
                 unformatted = True, 
                 default_translation_content = 'Default translation content', 
--- test/test_project_update_parameters.py
+++ test/test_project_update_parameters.py
@@ -43,7 +43,7 @@ class TestProjectUpdateParameters(unittest.TestCase):
                 main_format = 'yml', 
                 media = 'Python', 
                 shares_translation_memory = True, 
-                project_image = '[B@22d7e121', 
+                project_image = '[B@6b3decaa', 
                 remove_project_image = False, 
                 workflow = 'review', 
                 machine_translation_enabled = True, 

d40252b0a9d118531892a48a02a451ae2dc76580
Phrase <support@phrase.com>
2026-05-15 13:52:47 +0000

    Deploying from  phrase/openapi@e01c1a2c

--- test/test_project_update_parameters.py
+++ test/test_project_update_parameters.py
@@ -43,7 +43,7 @@ class TestProjectUpdateParameters(unittest.TestCase):
                 main_format = 'yml', 
                 media = 'Python', 
                 shares_translation_memory = True, 
-                project_image = '[B@2c8d537e', 
+                project_image = '[B@24fa43f8', 
                 remove_project_image = False, 
                 workflow = 'review', 
                 machine_translation_enabled = True, 
